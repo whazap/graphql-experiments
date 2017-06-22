6d8c754f3d1a95e5376b9a436651139f0a0cbc75
Sebastian Herber <sh@zaplab.at>
2017-06-23 01:34:06 +0200

    Testing multiple requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,50 @@ import { gql, graphql } from 'react-apollo';
 import MediaList from './MediaList';
 import './App.css';
 
+const MediaListWithData1 = graphql(gql`
+    query {
+        medias(limit: 1) {
+            status,
+            thumbnail
+        }
+    }
+`)(MediaList);
+
+const MediaListWithData2 = graphql(gql`
+    query {
+        medias(limit: 2) {
+            status,
+            thumbnail
+        }
+    }
+`)(MediaList);
+
+const MediaListWithData3 = graphql(gql`
+    query {
+        medias(limit: 3) {
+            status,
+            thumbnail
+        }
+    }
+`)(MediaList);
+
+const MediaListWithData4 = graphql(gql`
+    query {
+        medias(limit: 4) {
+            status,
+            thumbnail
+        }
+    }
+`)(MediaList);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <MediaList
-              data={{
-                  loading: false,
-                  medias: [
-                      {
-                          status: '1',
-                          thumbnail: '/static/img/media/live/sscaitournament_mid_000.jpg',
-                      },
-                      {
-                          status: '2',
-                          thumbnail: '/static/img/media/live/sscaitournament_mid_000.jpg',
-                      },
-                      {
-                          status: '3',
-                          thumbnail: '/static/img/media/live/sscaitournament_mid_000.jpg',
-                      },
-                  ],
-              }}
-          />
+          <MediaListWithData1 />
+          <MediaListWithData2 />
+          <MediaListWithData3 />
+          <MediaListWithData4 />
       </div>
     );
   }
